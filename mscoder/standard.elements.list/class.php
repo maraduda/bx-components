f9860dad29ee7cd9bcb93da18bfe9e673985ab1e
Andrey Maraduda <andrey@maraduda.com>
2014-05-25 22:32:26 +0400

    Переименован класс

--- mscoder/standard.elements.list/class.php
+++ mscoder/standard.elements.list/class.php
@@ -4,7 +4,7 @@ if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 use \Bitrix\Main;
 use \Bitrix\Main\Localization\Loc as Loc;
 
-class RealesesList extends CBitrixComponent
+class StandardElementListComponent extends CBitrixComponent
 {
 	/**
 	 * кешируемые ключи arResult
